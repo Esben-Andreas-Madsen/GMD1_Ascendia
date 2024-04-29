8bb8076c8a77381cc88755b748b2f94deeead345
beva-trifork <302301@viauc.dk>
2024-04-29 18:14:44 +0200

    Display floor-level ingame
    Added text to display what level of platform the player has reached.

--- Ascendia/Assets/Scripts/FloorTracker.cs
+++ Ascendia/Assets/Scripts/FloorTracker.cs
@@ -2,28 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using TMPro;
 
 public class FloorTracker : MonoBehaviour
 {
     private int currentFloor = 0;
+    public TextMeshProUGUI floorCountText;
 
-    private void OnTriggerEnter2D(Collider2D other)
+
+    /* private void OnTriggerEnter2D(Collider2D other)
+     {
+         if (other.CompareTag("Platform"))
+         {
+             currentFloor++;
+             UnityEngine.Debug.Log("Current floor: " + currentFloor);
+         }
+     } */
+
+    private void OnTriggerExit2D(Collider2D other)
     {
+        /* if (other.CompareTag("Platform"))
+        {
+            currentFloor--;
+            UnityEngine.Debug.Log("Current floor: " + currentFloor);
+        } */
         if (other.CompareTag("Platform"))
         {
             currentFloor++;
             UnityEngine.Debug.Log("Current floor: " + currentFloor);
         }
-    }
-
-   /* private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Platform"))
-        {
-            currentFloor--;
-            UnityEngine.Debug.Log("Current floor: " + currentFloor);
-        }
-    } */
+    } 
 }
 
 

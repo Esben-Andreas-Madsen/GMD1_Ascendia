ab4351af488d79f8bbd09633251f451c5e4c387d
Esben-Andreas-Madsen <91538845+Esben-Andreas-Madsen@users.noreply.github.com>
2024-05-27 15:11:16 +0200

    Revert "trying to resolve merge conflicts"

---
+++ Ascendia/Assets/Sprites/MovingObstacle2D.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public class MovingObstacle2D : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float rotationSpeed = 100f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private GameObject player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(ChangeDirection());
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        movement.y = Mathf.Min(0f, movement.y);
+
+        Vector2 newPosition = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
+    }
+
+    private void Rotate()
+    {
+        if (movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+            float currentAngle = Mathf.Atan2(rb.transform.up.y, rb.transform.up.x) * Mathf.Rad2Deg;
+            float angleDifference = Mathf.DeltaAngle(currentAngle, targetAngle);
+
+            float rotationAmount = Mathf.Sign(angleDifference) * Mathf.Min(rotationSpeed * Time.fixedDeltaTime, Mathf.Abs(angleDifference));
+            rb.MoveRotation(rb.rotation + rotationAmount);
+        }
+    }
+
+    private IEnumerator ChangeDirection()
+    {
+        while (true)
+        {
+            if (player != null)
+            {
+                Vector2 directionToPlayer = (player.transform.position - transform.position).normalized;
+                movement = directionToPlayer;
+            }
+            yield return new WaitForSeconds(Random.Range(0.25f, 1f));
+        }
+    }
+}
